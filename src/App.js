213d1dbe21545b1b13b48dc0d7fe2a2d82a7ae31
Chacko jithin <jithinchackopayyanat@gmail.com>
2022-10-07 22:57:08 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
   import './App.css';
 import Header from './Components';
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './Pages/Home';
+import Dashboard from './Pages/Dashboard';
+import Search from './Pages/Search';
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <h1>Hi</h1>
-    </div>
+     <BrowserRouter>
+       <Routes>
+         <Route path='/' element={<Home/>}/>
+         <Route path='/dashboard' element={<Dashboard/>}/>
+         <Route path='/search' element={<Search/>}/>
+       </Routes>
+     </BrowserRouter>
   );
 }
 
