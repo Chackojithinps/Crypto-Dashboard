5e288b70526a9b3e2fb5ebe2c5fd419af1294ea6
Chacko jithin <jithinchackopayyanat@gmail.com>
2022-10-23 22:19:16 +0530

    data-div changes

--- src/Components/DashboardComponents/Grid/index.js
+++ src/Components/DashboardComponents/Grid/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './styles.css'
+import TrendingUpRoundedIcon from '@mui/icons-material/TrendingUpRounded';
+import TrendingDownRoundedIcon from '@mui/icons-material/TrendingDownRounded';
 const Grid = ({coin}) => {
   return (
     <div className='coin-box'>
@@ -10,6 +12,41 @@ const Grid = ({coin}) => {
              <p className='coin-name'>{coin.name}</p>
           </div>
        </div>
+       <div className='data-div'>
+          {/* <div className='coin-chip'
+              style={{color:coin.price_change_percentage_24h>0?"var(--green)":"red",
+                       borderColor:coin.price_change_percentage_24h>0?"var(--green)":"red"}}>
+            <p>{coin.price_change_percentage_24h>0?"+"+coin.price_change_percentage_24h.toFixed(2)+" %": coin.price_change_percentage_24h.toFixed(2)+" %"}</p>
+         </div> */}
+         {/* <div className='coin-trendIcon' 
+            style={{borderColor:coin.price_change_percentage_24h>0?"var(--green)":"var(--red)"}}>
+            {coin.price_change_percentage_24h>0?
+            <div className='green'>
+                <TrendingUpRoundedIcon className='green' style={{color:"var(--green)",borderColor:"var(--green)"}}/>
+            </div>
+           :
+           <div className='red'>
+             <TrendingDownRoundedIcon style={{color:"var(--red)",borderColor:"var(--green)"}}/>
+            </div>
+           }
+         </div> */}
+         <div className='chip'>
+             {coin.price_change_percentage_24h>0?
+                <div className='upChip'>{"+"+coin.price_change_percentage_24h.toFixed(2)+" %"}</div>:
+                <div className='downChip'>{coin.price_change_percentage_24h.toFixed(2)+" %"}</div>
+             }
+         </div>
+         <div className='trendIcon'>
+             {coin.price_change_percentage_24h>0?
+               <div className='upTrend'>
+                   <TrendingUpRoundedIcon/>
+               </div>:
+               <div className='downTrend'>
+                    <TrendingDownRoundedIcon/>
+              </div>
+             }
+         </div>
+      </div>
     </div>
   )
 }
