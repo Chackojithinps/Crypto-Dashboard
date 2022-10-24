c917cc44956e0444a1d3c1c8664adc7981df8134
Chacko jithin <jithinchackopayyanat@gmail.com>
2022-10-24 22:31:31 +0530

    partial responsive list dashboard

--- src/Components/DashboardComponents/List/index.js
+++ src/Components/DashboardComponents/List/index.js
@@ -7,38 +7,39 @@ const List = ({coin}) => {
       <tr className='list-coin-box'>
        <td className='list-logo-div min'>
           <img className='list-logo' src={coin.image}/>
+     
           <div className='coin-info'>
              <p className='coin-symbol text-td'>{coin.symbol}-USD</p>
-             <p className='coin-name text-td'>{coin.name}</p>
+             <p className='coin-name text-td coin-name'>{coin.name}</p>
           </div>
        </td>
        <td className='list-data-div min'>
           
-         <div className='chip text-td'>
+         <div className='chip'>
              {coin.price_change_percentage_24h>0?
-                <div className='upChip'>{"+"+coin.price_change_percentage_24h.toFixed(2)+" %"}</div>:
-                <div className='downChip'>{coin.price_change_percentage_24h.toFixed(2)+" %"}</div>
+                <div className='upChip text-td chip-td'>{"+"+coin.price_change_percentage_24h.toFixed(2)+" %"}</div>:
+                <div className='downChip text-td chip-td'>{coin.price_change_percentage_24h.toFixed(2)+" %"}</div>
              }
          </div>
-         <div className='trendIcon text-td'>
+         <div className='trendIcon'>
              {coin.price_change_percentage_24h>0?
-               <div className='upTrend'>
+               <div className='upTrend trend-td'>
                    <TrendingUpRoundedIcon/>
                </div>:
-               <div className='downTrend'>
+               <div className='downTrend trend-td'>
                     <TrendingDownRoundedIcon/>
               </div>
              }
          </div>
       </td>
-      <td className='list-coin-price min text-td'>
+      <td className='list-coin-price min '>
          {coin.price_change_percentage_24h>0?
-           <p className='price-up'>${coin.current_price.toFixed(2)}</p>
-           :<p className='price-down'>${coin.current_price}</p>
+           <p className='price-up text-td'>${coin.current_price.toFixed(2)}</p>
+           :<p className='price-down text-td'>${coin.current_price}</p>
          }
       </td>
      
-       <td className='coin-vol vol min text-td'> ${coin.total_volume.toLocaleString()}</td>
+       <td className='coin-vol markt_cap min text-td'> ${coin.total_volume.toLocaleString()}</td>
        <td className='coin-marketCap vol min text-td'> ${coin.market_cap.toLocaleString()}</td>
     
     </tr>
