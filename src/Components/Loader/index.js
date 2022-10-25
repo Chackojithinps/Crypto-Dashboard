6b232b32d0530b9128dd335b6362c5a474bde7aa
Chacko jithin <jithinchackopayyanat@gmail.com>
2022-10-25 13:03:29 +0530

    loader

---
+++ src/Components/Loader/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CircularProgress from '@mui/material/CircularProgress';
+import './styles.css'
+const Loader1 = () => {
+  return (
+    <div className='loader1'>
+        <CircularProgress/>
+    </div>
+  )
+}
+
+export default Loader1
--- src/Pages/CoinPages.js
+++ src/Pages/CoinPages.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import Loader1 from '../Components/Loader';
 import Header from '../Components/Header'
-
+// import Loader1 from '../Components/Loader'
 const CoinPages = () => {
     const [searchParams]=useSearchParams();
-    console.log(searchParams)
+    const [data,setData]=useState([])
+    const [loading,setLoading]=useState(true)
+    // useEffect(()=>{
+    //     const API_URL="https://api.coingecko.com/api/v3/coins/"+searchParams;
+    //     axios.get(API_URL.slice(0,-1)).then((res)=>{
+    //         if(res.data){
+    //             console.log(res.data)
+    //             setData(res.data)
+    //             setLoading(false)
+    //         }else{
+    //             console.log("error")
+    //         }
+           
+    //     })
+    // },[searchParams])
   return (
     <div>
-       <Header/>
-       <h1>{searchParams}</h1>
+       
+       <div className='kkkk'>
+          
+          {loading?<Loader1/>:
+          <>
+          <Header/>
+          <h1>got the ans</h1>
+          </>}
+       </div>
     </div>
   )
 }
