4cfe9f12427bf21ecc84beef6c207259f9262844
Chacko jithin <jithinchackopayyanat@gmail.com>
2022-11-02 17:06:25 +0530

    chart changes

--- src/Components/DashboardComponents/LineChart/index.js
+++ src/Components/DashboardComponents/LineChart/index.js
@@ -18,10 +18,10 @@ import "./styles.css";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
-function LineChart({ chartData, options }) {
+function LineChart({ chartData , options}) {
   return (
     <div>
-      <Line data={chartData} options={options} />
+      <Line data={chartData} options={options}/>
     </div>
   );
 }
--- src/Pages/CoinPages.js
+++ src/Pages/CoinPages.js
@@ -8,6 +8,9 @@ import ChartList from '../Components/DashboardComponents/ChartList'
 import { Chart as ChartJS } from "chart.js/auto";
 
 
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 
 const CoinPages = () => {
     const [searchParams]=useSearchParams();
@@ -15,18 +18,40 @@ const CoinPages = () => {
     const [loading,setLoading]=useState(true)
     const [prices,setPrices]=useState([])
     const [loadingChart,setLoadingChart]=useState(true)
-    const [chartData,setChartData]=useState({})
-    const [days, setDays] = React.useState(30);
-
-    const handleChange = (event) => {
-        console.log("value///",event)
-        setDays(event.target.value);
-      }; 
-
+    const [chartData, setChartData] = useState({
+        labels: [],
+        datasets: [
+          {
+            data: [],
+            borderWidth: 2,
+            fill: false,
+            tension: 0.25,
+            backgroundColor: "white",
+            borderColor: "white",
+            pointRadius: 0,
+          },
+        ],
+      });
+      const [days, setDays] = React.useState(30);
+      const options = {
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      };
+    ;
     const today=new Date();
     const priorDate=new Date(new Date().setDate(today.getDate()-days))
     
-    const getDateArray=(start,end)=>{
+    const getDaysArray=(start,end)=>{
+        // for(var a=[],d=new Date(start);
+        //   d<=new Date(end);
+        //   d.setDate(d.getDate()+1)
+        // ){
+        //     a.push(new Date(d).getDate() + '/' +(new Date(d).getUTCMonth()+1))
+        // }
+        // return a;
         var arr=new Array();
         var dt=new Date(start)
         while(dt<=end){
@@ -35,49 +60,96 @@ const CoinPages = () => {
         }
         return arr;
     }
-   
-    useEffect(()=>{
-        const API_URL="https://api.coingecko.com/api/v3/coins/"+searchParams;
-         axios
-          .get(API_URL.slice(0,-1),{crossDomain:true})   //here in this link last an = sighn came .so to avoid we use slice here.
-          .then((res)=>{ 
-            if(res.data){
-                console.log(res.data)
-                setData(res.data)
-                const API_URL2=`https://api.coingecko.com/api/v3/coins/${res.data.id}/market_chart?vs_currency=usd&days=${days}&interval=daily`;
-                    axios.get(API_URL2).then((response)=>{
-                        if(response.data){
-                            console.log(response.data)
-                            setPrices(response.data.prices)
-                            setLoadingChart(false)
-                            setLoading(false)
-                            setChartData({
-                                  labels: getDateArray(priorDate,today),
-                                  datasets:[
-                                      {
-                                         data:prices?.map((data)=>data[1]),
-                                         borderWidth:2,
-                                         fill:false,
-                                         tension:0.25,
-                                         backgroundColor:"white",
-                                         borderColor:"white",
-                                         pointRadious:0,
-                                      }
-                                  ]
-                                }
-                            )
-                        }else{
-                            console.log("error")
-                        }
-                       
-                    })
-                
-            }else{
-                console.log("error")
+    useEffect(() => {
+        if (searchParams) {
+          getData();
+        }
+      }, [searchParams]);
+    
+      const getData = async () => {
+        const API_URL = `https://api.coingecko.com/api/v3/coins/${searchParams}`;
+    
+        const response_data = await axios.get(API_URL.slice(0, -1), {
+          crossDomain: true,
+        });
+    
+        if (!response_data) {
+          console.log("No data");
+          return;
+        }
+        setData(response_data.data);
+    
+        const API_URL2 = `https://api.coingecko.com/api/v3/coins/${response_data.data.id}/market_chart?vs_currency=usd&days=${days}&interval=daily`;
+    
+        const prices_data = await axios.get(API_URL2, {
+          crossDomain: true,
+        });
+    
+        if (!prices_data) {
+          console.log("No price data");
+          return;
+        }
+    
+        setPrices(prices_data.data.prices);
+    
+        var dates_2 = getDaysArray(priorDate, today);
+    
+        setChartData({
+          labels: dates_2,
+          datasets: [
+            {
+              data: prices_data?.data?.prices?.map((data) => data[1]),
+              borderWidth: 2,
+              fill: false,
+              tension: 0.25,
+              backgroundColor: "white",
+              borderColor: "white",
+              pointRadius: 0,
+            },
+          ],
+        });
+    
+        setLoadingChart(false);
+        setLoading(false);
+     }
+        const handleChange = async (event) => {
+            
+            setDays(event.target.value);
+            const API_URL2 = `https://api.coingecko.com/api/v3/coins/${data.id}/market_chart?vs_currency=usd&days=${event.target.value}&interval=daily`;
+        
+            const prices_data = await axios.get(API_URL2, {
+              crossDomain: true,
+            });
+        
+            if (!prices_data) {
+              console.log("No price data");
+              return;
             }
-           
-        })
-    },[searchParams])
+        
+            setPrices(prices_data.data.prices);
+        
+            const priorDate_2 = new Date(
+              new Date().setDate(today.getDate() - event.target.value)
+            );
+        
+            var dates_2 = getDaysArray(priorDate_2, today);
+        
+            setChartData({
+              labels: dates_2,
+              datasets: [
+                {
+                  data: prices_data?.data?.prices?.map((data) => data[1]),
+                  borderWidth: 2,
+                  fill: false,
+                  tension: 0.25,
+                  backgroundColor: "white",
+                  borderColor: "white",
+                  pointRadius: 0,
+                },
+              ],
+            });
+      }
+   
    return (
     <div>
        
@@ -88,22 +160,23 @@ const CoinPages = () => {
           <>
           <Header/>
           <ChartList coin={data}/>
-          {/* <BasicSelect handleChange={handleChange} age={age}/> */}
-          <form className='selectForm'>
-            <label for="cars">Price Change in the last</label>
-            <select onChange={handleChange}
-            className="selectInput" id="cars" name="cars">
-                <option value={30}>30 Days</option>
-                <option value={60}>60 Days</option>
-                <option value={90}>90 Days</option>
-                <option value={7}>7 Days</option>
-            </select>
-          </form>
-          <LineChart chartData={chartData}/>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={days}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={30}>30</MenuItem>
+          <MenuItem value={60}>60</MenuItem>
+          <MenuItem value={90}>90</MenuItem>
+        </Select>
+          <LineChart chartData={chartData} options={options}/>
           </>
          }
        </div>
     </div>
   )
-}
+
 export default CoinPages
